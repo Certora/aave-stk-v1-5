43899b30c6b28df4d8c823b3c82c6a0511d07197
Tadeas Kucera <tadeas@certora.com>
2023-03-09 16:05:06 +0100

    Remove Governance harness incl. references from specs

--- certora/scripts/runInvariants.sh
+++ certora/scripts/runInvariants.sh
@@ -5,14 +5,12 @@ fi
 
 certoraRun certora/harness/StakedAaveV3Harness.sol \
     certora/harness/DummyERC20Impl.sol \
-    certora/harness/AaveGovernance.sol \
     --link StakedAaveV3Harness:STAKED_TOKEN=DummyERC20Impl \
     --link StakedAaveV3Harness:REWARD_TOKEN=DummyERC20Impl \
-    --link StakedAaveV3Harness:_aaveGovernance=AaveGovernance \
     --verify StakedAaveV3Harness:certora/specs/invariants.spec \
     --solc solc8.17 \
     --cloud \
     --optimistic_loop \
     --loop_iter 3 \
     --rules $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} \
-    --msg "invariants" 
+    --msg "invariants"
