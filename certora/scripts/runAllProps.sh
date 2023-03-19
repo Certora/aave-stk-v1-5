e322a2702d86a89ad0d75f4a27c79769ea037456
Michael M <MichaelM@certora.com>
2023-03-19 12:31:58 +0200

    fixing allProps script + renewing patch

--- certora/scripts/runAllProps.sh
+++ certora/scripts/runAllProps.sh
@@ -1,8 +1,3 @@
-if [[ "$2" ]]
-then
-    RULE="--rule $2"
-fi
-
 certoraRun certora/harness/StakedAaveV3Harness.sol \
     certora/harness/DummyERC20Impl.sol \
     certora/harness/RewardVault.sol \
@@ -14,6 +9,6 @@ certoraRun certora/harness/StakedAaveV3Harness.sol \
     --cloud \
     --optimistic_loop \
     --loop_iter 3 \
-    --rules $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21} ${22} ${23} ${24} \
+    --rules $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21} ${22} ${23} ${24} \
     --settings -t=600 \
-    --msg "all props $1"
+    --msg "all props"
